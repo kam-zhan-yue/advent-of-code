082cf2e5999699deccc3be256c089ccbf67f7858
Alex Kam <kamzhanyue@gmail.com>
2025-12-03 03:17:40 +0900

    unfortunately solve in python :(

---
+++ 2025/02/solution.py
@@ -0,0 +1,57 @@
+test_input = open('tests/test-input.txt').read()
+real_input = open('tests/real-input.txt').read()
+
+def get_digits(num: int):
+    digits = 1
+    val = num // 10
+    while val > 0:
+        val = val // 10
+        digits += 1
+    return digits
+
+def part_one(input: str):
+    total = 0
+    pairs = input.split(',')
+    for pair in pairs:
+        nums = pair.split('-')
+        left = int(nums[0])
+        right = int(nums[1])
+        for i in range(right - left + 1):
+            num = left + i
+            digits = get_digits(num)
+            if digits % 2 != 0:
+                continue
+            if num % (pow(10, digits // 2) + 1) == 0:
+                total += num
+
+    print("Part One is:", total)
+
+
+def is_repeat(word: str) -> bool:
+    for i in range(1, len(word) // 2 + 1):
+        sub = word[0:i]
+        repeat = len(word) // i
+        if sub * repeat == word:
+            return True
+    return False
+
+
+def part_two(input: str):
+    total = 0
+    pairs = input.split(',')
+    for pair in pairs:
+        nums = pair.split('-')
+        left = int(nums[0])
+        right = int(nums[1])
+        for i in range(right - left + 1):
+            num = left + i
+            if is_repeat(str(num)):
+                total += num
+
+    print("Part Two is:", total)
+
+part_one(test_input)
+part_one(real_input)
+
+part_two(test_input)
+part_two(real_input)
