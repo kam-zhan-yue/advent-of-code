a7b8b1cb65cb4e9653f77765dd10ce24130c5bd5
Alex Kam <kamzhanyue@gmail.com>
2025-12-04 18:57:37 +0900

    solve 2025/04 part 1 in bash

--- 2025/04/part-one.sh
+++ 2025/04/part-one.sh
@@ -1,32 +1,57 @@
 #!/usr/bin/env bash
 
+# Initialise associate array (2D array hack)
 declare -A matrix # associative array
-
 rows=0
 cols=0
-
 while read line
 do
   cols=0
   entry=()
   for (( i=0; i<${#line}; ++i )); do
-    # echo "${line:$i:1} to ${rows}, ${cols}"
-    if [[ ${rows} == 0 && ${cols} == 0 ]]; then
-      echo "${line:$i:1} to ${rows}, ${cols}"
-      echo "${matrix[${rows},${cols}]}"
-    fi
     matrix["${rows},${cols}"]=${line:$i:1}
-    # echo "${matrix[${rows},${cols}]}"
     ((cols++))
   done
   ((rows++))
 done
 
-echo "${rows}"
-echo "${cols}"
+
+directions=(
+  "-1,-1",
+  "-1,0",
+  "-1,1",
+  "0,1",
+  "0,-1",
+  "1,1",
+  "1,0",
+  "1,-1",
+)
+
+get_neighbours() {
+  neighbours=0
+  for (( i=0; i<${#directions[@]}; ++i )); do
+    vector=${directions[i]}
+    # https://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
+    values=(${vector//,/ })
+    x=$(( ${values[0]}+${1} ))
+    y=$(( ${values[1]}+${2} ))
+    if [[ ${matrix[${x},${y}]} == "@" ]]; then
+      ((neighbours++))
+    fi
+  done
+  echo ${neighbours}
+}
+
+total=0
 for (( i=0; i<${rows}; ++i )) do
   for (( j=0; j<${cols}; ++j )) do
-    printf "${matrix[${i},${j}]}"
+    if [[ ${matrix[${i},${j}]} == "@" ]]; then
+      neighbours=$(get_neighbours ${i} ${j})
+      if [[ ${neighbours} < 4 ]]; then
+        ((total++))
+      fi
+    fi
   done
-  printf "\n"
 done
+
+echo "Total is ${total}"
