e77ef374b460f2bc0d90b63f228f37e66b756378
Alex Kam <kamzhanyue@gmail.com>
2025-12-06 15:47:34 +0900

    solve 2025/06 in c++

--- 2025/06/solution.cpp
+++ 2025/06/solution.cpp
@@ -1,24 +1,105 @@
-#include <iostream>
 #include <qaz_kosu_kosu.h>
 
 қолдану кеңістік стандартты_кіріспе;
 
-елбасы() {
-    // Кодты мында енгізіңіз
+const char DELIM = ' ';
 
-    // Тез cұрыптау функциясының қолданылу мысалы
-    вектор_сан v = { 9, 7, 2, 3, 700, 0, -91, 91, 10, -5 };
-    тез_сұрыптау(v);
-    төңкеру(барлығы(v));
-    үшін(сан i = 0; i < v.ұзындығы(); i++) {
-        шығыс << v[i] << " ";
+struct Problem {
+  vector<string> numbers;
+  vector<long> longs;
+  bool addition;
+};
+
+vector<string> get_lines() {
+  vector<string> lines;
+  string line;
+  while(std::getline(std::cin, line)) {
+    lines.push_back(line);
+  }
+  return lines;
+}
+
+int empty_column(vector<string> lines, int col) {
+  for (int i=0; i<lines.size(); ++i) if (lines[i][col] != ' ') return false;
+  return true;
+}
+
+vector<Problem> get_problems() {
+  vector<string> lines = get_lines();
+  vector<Problem> problems;
+
+  int rows = lines.size();
+  int cols = lines[0].size();
+
+  int index = 0;
+  for (int i=0; i<cols; ++i) {
+    if (!empty_column(lines, i) && i != cols-1) continue;
+    vector<string> numbers;
+    vector<long> longs;
+    int len = i == cols-1 ? i-index + 1 : i-index;
+    // Ignore the operation layer
+    for (int j=0; j<rows-1; ++j) {
+      string number = lines[j].substr(index, len);
+      numbers.push_back(number);
+      longs.push_back(stol(number));
     }
-    шығыс << жаңа_жол;
+    string operation_row = lines[rows-1].substr(index, len);
+    bool addition = operation_row.find('+') != string::npos;
 
-    // Бинарлы дәрежеге шығару мысалы
-    сан x = 127;
-    сан y = 25;
-    шығыс << бинарлы_дәреже(x, y) << жаңа_жол;
+    index = i + 1;
+    problems.push_back({ numbers, longs, addition });
+  }
+  return problems;
+}
 
-    қайтару 0;
+void part_one(vector<Problem> problems) {
+  int line_num = 0;
+  long total = 0;
+
+  for (int i=0; i<problems.size(); ++i) {
+    Problem problem = problems[i];
+    long val = problem.longs[0];
+    for (int j=1; j<problem.longs.size(); ++j) {
+      if (problem.addition) val += problem.longs[j];
+      else val *= problem.longs[j];
+    }
+    total += val;
+  }
+  cout << "Part One is " << total << endl;
+}
+
+long solve_problem(Problem problem) {
+  int rows = problem.numbers.size();
+  int cols = problem.numbers[0].size();
+  vector<string> numbers(cols);
+
+  for (int i=0; i<cols; ++i) {
+    for (int j=0; j<rows; ++j) {
+      if (problem.numbers[j][i] == ' ') continue;
+      numbers[i] += problem.numbers[j][i];
+    }
+  }
+
+  long total = stol(numbers[0]);
+  for (int i=1; i<numbers.size(); ++i) {
+    if (problem.addition) total += stol(numbers[i]);
+    else total *= stol(numbers[i]);
+  }
+  return total;
+  return 0;
+}
+
+void part_two(vector<Problem> problems) {
+  long total = 0;
+  for (int i=0; i<problems.size(); ++i) {
+    total += solve_problem(problems[i]);
+  }
+  cout << "Part Two is " << total << endl;
+}
+
+елбасы() {
+  vector<Problem> problems = get_problems();
+  part_one(problems);
+  part_two(problems);
+  return 0;
 }
