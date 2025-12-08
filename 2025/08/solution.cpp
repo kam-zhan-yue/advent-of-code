54f9087618637d78257d708a5e7c34c8c5ec23b3
Alex Kam <kamzhanyue@gmail.com>
2025-12-08 16:02:57 +0900

    solve 2025/08 part one in c++

---
+++ 2025/08/solution.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+struct Point {
+  long x;
+  long y;
+  long z;
+};
+
+class Graph {
+private:
+  vector<Point> points;
+  map<int, int> hashmap;
+  map<int, vector<int>> circuits;
+  int circuit_num = 0;
+
+public:
+  Graph(vector<Point> p) {
+    points = p;
+  }
+
+  int connect(int a, int b) {
+    // If either are in the graph and in the same circuit, then do nothing
+    if (hashmap.count(a) && hashmap.count(b) && hashmap.at(a) == hashmap.at(b)) {
+      return 1;
+    }
+
+    // If neither are in the graph, then init them together
+    if (!hashmap.count(a) && !hashmap.count(b)) {
+      circuits.insert({ circuit_num, { a, b } });
+      hashmap.insert({ a, circuit_num });
+      hashmap.insert({ b, circuit_num });
+      circuit_num++;
+      return 1;
+    }
+
+    if (hashmap.count(a) && !hashmap.count(b)) {
+      int circuit_id = hashmap[a];
+      hashmap.insert({ b, circuit_id });
+      circuits[circuit_id].push_back(b);
+      return 1;
+    }
+    if (hashmap.count(b) && !hashmap.count(a)) {
+      int circuit_id = hashmap[b];
+      hashmap.insert({ a, circuit_id });
+      circuits[circuit_id].push_back(a);
+      return 1;
+    }
+
+    // Then they are in two different circuits and we need to merge them
+    int ca = hashmap[a];
+    int cb = hashmap[b];
+    if (circuits[ca].size() < circuits[cb].size()) {
+      swap(ca, cb);
+    }
+    for (int node : circuits[cb]) {
+      circuits[ca].push_back(node);
+      hashmap[node] = ca;
+    }
+    circuits.erase(cb);
+    return 1;
+  }
+
+  int connected_circuits() {
+    return circuits.size();
+  }
+
+  long get_part_one() {
+    long part_one = 1;
+    vector<size_t> sizes;
+    for (const auto& [key, vec] : circuits) {
+      sizes.push_back(vec.size());
+    }
+
+    sort(sizes.begin(), sizes.end(), greater<>());
+
+    for (int i=0; i<3; ++i) {
+      part_one *= sizes[i];
+    }
+    return part_one;
+  }
+};
+
+
+array<string_view, 2> split(string_view str, char delimiter) {
+  array<string_view, 2> tokens;
+  size_t start = 0;
+  size_t end = str.find(delimiter);
+  tokens[0] = (str.substr(start, end));
+  tokens[1] = (str.substr(end, str.size()));
+  return tokens;
+}
+
+int get_connections() {
+  string line;
+  getline(cin, line);
+  cout << "Connections: " << line << endl;
+  return stol(line);
+}
+
+vector<Point> get_points() {
+  vector<Point> points;
+  string line;
+  while(std::getline(std::cin, line)) {
+    size_t split_1 = line.find(',');
+    size_t split_2 = line.find(',', split_1 + 1);
+
+    string x = line.substr(0, split_1);
+    string y = line.substr(split_1 + 1, split_2 - split_1 - 1);
+    string z = line.substr(split_2 + 1);
+
+    Point point = {
+      stol(x),
+      stol(y),
+      stol(z),
+    };
+    
+    points.push_back(point);
+
+  }
+  return points;
+}
+
+long long length(Point a, Point b) {
+  long long dx = (long long)a.x - b.x;
+  long long dy = (long long)a.y - b.y;
+  long long dz = (long long)a.z - b.z;
+  return dx*dx + dy*dy + dz*dz;
+}
+
+// TODO: Optimise
+int get_closest_point(vector<Point> points, int index) {
+  long squared_distance = 0;
+  int min_index = -1;
+  for (int i=0; i<points.size(); ++i) {
+    if (i == index) continue;
+    long distance = length(points[index], points[i]);
+    if (min_index < 0) {
+      squared_distance = distance;
+      min_index = i;
+    } else if (distance < squared_distance) {
+      squared_distance = distance;
+      min_index = i;
+    }
+  }
+  return min_index;
+}
+
+void solve(vector<Point> points, int connections) {
+  // generate distances
+  multimap<long long, tuple<int, int>> distances;
+  for (int i=0; i<points.size(); ++i) {
+    for (int j=i+1; j<points.size(); ++j) {
+      long distance = length(points[i], points[j]);
+      distances.insert({ distance, { i, j} });
+    }
+  }
+
+  Graph graph(points);
+  int connected = 0;
+  for (const auto& [distance, pair] : distances) {
+    const auto& [p1, p2] = pair;
+    connected += graph.connect(p1, p2);
+    if (connected >= connections) break;
+  }
+
+
+  /*graph.print();*/
+
+  cout << "Connected Circuits: " << graph.connected_circuits() << endl;
+  cout << "Part One: " << graph.get_part_one() << endl;
+}
+
+int main() {
+  int connections = get_connections();
+  vector<Point> points = get_points();
+  solve(points, connections);
+}
+
