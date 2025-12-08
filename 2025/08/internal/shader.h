d97a013aed1b1f1bf2f33296c72bc77c11ed533e
Kam Zhan Yue <kamzhanyue@gmail.com>
2025-12-09 00:43:53 +0900

    split out logic into header files

---
+++ 2025/08/internal/shader.h
@@ -0,0 +1,167 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader {
+public:
+	// the program ID
+	unsigned int ID;
+	
+  Shader(const char* vertexPath, const char* geometryPath, const char* fragmentPath) {
+    unsigned int vertex = generateShader(vertexPath, GL_VERTEX_SHADER);
+    unsigned int geometry = generateShader(geometryPath, GL_GEOMETRY_SHADER);
+    unsigned int fragment = generateShader(fragmentPath, GL_FRAGMENT_SHADER);
+
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, geometry);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(ID, GL_LINK_STATUS, &success);
+    if (!success) {
+      glGetProgramInfoLog(ID, 512, NULL, infoLog);
+      std::cout << "ERROR:SHADER::PROGRAM::LINKING_FAILURE\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertex);
+    glDeleteShader(geometry);
+    glDeleteShader(fragment);
+  }
+
+	// constructor reads and builds the shader
+  Shader(const char* vertexPath, const char* fragmentPath) {
+    unsigned int vertex = generateShader(vertexPath, GL_VERTEX_SHADER);
+    unsigned int fragment = generateShader(fragmentPath, GL_FRAGMENT_SHADER);
+
+    // 3 - Link Shader
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(ID, GL_LINK_STATUS, &success);
+    if (!success) {
+      glGetProgramInfoLog(ID, 512, NULL, infoLog);
+      std::cout << "ERROR:SHADER::PROGRAM::LINKING_FAILURE\n" << infoLog << std::endl;
+    }
+
+    // Delete shaders as they're linked to the program and are no longer necessary
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+  }
+
+  // to activate the shader
+  void use() {
+    glUseProgram(ID);
+  }
+
+  // utility functions
+  void setBool(const std::string &name, bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+  }
+
+  void setInt(const std::string &name, int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+  }
+
+  void setFloat(const std::string &name, float value) const {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+  };
+
+  void setVec2(const std::string &name, float x, float y) const {
+    glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+  };
+
+  void setVec2(const std::string &name, glm::vec2 value) const {
+    glUniform2f(glGetUniformLocation(ID, name.c_str()), value.x, value.y);
+  };
+
+  void setVec3(const std::string &name, float x, float y, float z) const {
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+  };
+
+  void setVec3(const std::string &name, glm::vec3 value) const {
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), value.x, value.y, value.z);
+  };
+
+  void setMat4(const std::string &name, glm::mat4 value) const {
+    unsigned int loc = glGetUniformLocation(ID, name.c_str());
+    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(value));
+  };
+
+private:
+  unsigned int generateShader(const char *path, unsigned int SHADER_TYPE) {
+    // 1: Retrieve the vertex / fragment source code from the filepaths
+    std::string shaderCode;
+    std::ifstream shaderFile;
+    
+    // Ensure ifstream objects can throw exceptions
+    shaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+    
+    try {
+      // Open files
+      shaderFile.open(path);
+    
+      // Read files' buffer contents into stream
+      std::stringstream shaderStream;
+      shaderStream << shaderFile.rdbuf();
+    
+      // Close file handlers
+      shaderFile.close();
+      
+      // Convert stream into string
+      shaderCode = shaderStream.str();
+    } catch(std::ifstream::failure e) {
+      std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+    }
+
+    const char* code = shaderCode.c_str();
+    // 2 - Compile Shaders
+    unsigned int shader;
+    int success;
+    char infoLog[512];
+
+    // Vertex Shader
+    shader = glCreateShader(SHADER_TYPE);
+    glShaderSource(shader, 1, &code, NULL);
+    glCompileShader(shader);
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(shader, 512, NULL, infoLog);
+      std::string shaderTypeStr;
+      switch (SHADER_TYPE) {
+        case GL_VERTEX_SHADER:
+          shaderTypeStr = "VERTEX";
+          break;
+        case GL_FRAGMENT_SHADER:
+          shaderTypeStr = "FRAGMENT";
+          break;
+        case GL_GEOMETRY_SHADER:
+          shaderTypeStr = "GEOMETRY";
+          break;
+        default:
+          shaderTypeStr = "UNKNOWN";
+          break;
+      }
+      std::cout << "ERROR:SHADER::VERTEX::COMPILATION_FAILURE\n" << infoLog << std::endl;
+    }
+
+    return shader;
+    return 0;
+  }
+};
+
+#endif
--- 2025/08/solution.cpp
+++ 2025/08/solution.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <map>
+#include <algorithm.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-
-using namespace std;
-
-struct Point {
-  long x;
-  long y;
-  long z;
-};
+#include <shader.h>
 
 
 // OpenGL Stuff
@@ -18,89 +11,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window, float &deltaTime);
 void mouseCallback(GLFWwindow *window, double xPos, double yPos);
 void scrollCallback(GLFWwindow *window, double xPos, double yPos);
-
-// Algorithm Stuff
-int get_connections();
-void solve(vector<Point> points, int connections);
-int get_closest_point(vector<Point> points, int index);
-long long length(Point a, Point b);
-vector<Point> get_points();
-
-class Graph {
-private:
-  vector<Point> points;
-  map<int, int> hashmap;
-  map<int, vector<int>> circuits;
-  int circuit_num = 0;
-
-public:
-  Graph(vector<Point> p) {
-    points = p;
-  }
-
-  int connect(int a, int b) {
-    // If either are in the graph and in the same circuit, then do nothing
-    if (hashmap.count(a) && hashmap.count(b) && hashmap.at(a) == hashmap.at(b)) {
-      return 1;
-    }
-
-    // If neither are in the graph, then init them together
-    if (!hashmap.count(a) && !hashmap.count(b)) {
-      circuits.insert({ circuit_num, { a, b } });
-      hashmap.insert({ a, circuit_num });
-      hashmap.insert({ b, circuit_num });
-      circuit_num++;
-      return 1;
-    }
-
-    if (hashmap.count(a) && !hashmap.count(b)) {
-      int circuit_id = hashmap[a];
-      hashmap.insert({ b, circuit_id });
-      circuits[circuit_id].push_back(b);
-      return 1;
-    }
-    if (hashmap.count(b) && !hashmap.count(a)) {
-      int circuit_id = hashmap[b];
-      hashmap.insert({ a, circuit_id });
-      circuits[circuit_id].push_back(a);
-      return 1;
-    }
-
-    // Then they are in two different circuits and we need to merge them
-    int ca = hashmap[a];
-    int cb = hashmap[b];
-    if (circuits[ca].size() < circuits[cb].size()) {
-      swap(ca, cb);
-    }
-    for (int node : circuits[cb]) {
-      circuits[ca].push_back(node);
-      hashmap[node] = ca;
-    }
-    circuits.erase(cb);
-    return 1;
-  }
-
-  long get_part_one() {
-    long part_one = 1;
-    vector<size_t> sizes;
-    for (const auto& [key, vec] : circuits) {
-      sizes.push_back(vec.size());
-    }
-
-    sort(sizes.begin(), sizes.end(), greater<>());
-
-    for (int i=0; i<3; ++i) {
-      part_one *= sizes[i];
-    }
-    return part_one;
-  }
-
-  bool is_complete() {
-    vector<int> connected = circuits.begin()->second;
-    bool completed = connected.size() == points.size();
-    return completed;
-  }
-};
 
 GLFWwindow *init() {
   // Init GLFW and set the context variables
@@ -146,96 +56,11 @@ GLFWwindow *init() {
 
 int main() {
   GLFWwindow *window = init();
-  /*int connections = get_connections();*/
-  /*vector<Point> points = get_points();*/
-  /*solve(points, connections);*/
-
+  int connections = get_connections();
+  vector<Point> points = get_points();
+  solve(points, connections);
   return 0;
 }
-
-int get_connections() {
-  string line;
-  getline(cin, line);
-  return stol(line);
-}
-
-vector<Point> get_points() {
-  vector<Point> points;
-  string line;
-  while(std::getline(std::cin, line)) {
-    size_t split_1 = line.find(',');
-    size_t split_2 = line.find(',', split_1 + 1);
-
-    string x = line.substr(0, split_1);
-    string y = line.substr(split_1 + 1, split_2 - split_1 - 1);
-    string z = line.substr(split_2 + 1);
-
-    Point point = {
-      stol(x),
-      stol(y),
-      stol(z),
-    };
-    
-    points.push_back(point);
-
-  }
-  return points;
-}
-
-void solve(vector<Point> points, int connections) {
-  // generate distances
-  multimap<long long, tuple<int, int>> distances;
-  for (int i=0; i<points.size(); ++i) {
-    for (int j=i+1; j<points.size(); ++j) {
-      long distance = length(points[i], points[j]);
-      distances.insert({ distance, { i, j} });
-    }
-  }
-
-  Graph graph(points);
-  int connected = 0;
-  for (const auto& [distance, pair] : distances) {
-    const auto& [p1, p2] = pair;
-    connected += graph.connect(p1, p2);
-
-    if (connected == connections) {
-      cout << "Part One: " << graph.get_part_one() << endl;
-    }
-
-    // If we connected all of them, then break
-    if (graph.is_complete()) {
-      long long p1_x = points[p1].x;
-      long long p2_x = points[p2].x;
-      cout << "Part Two: " << p1_x * p2_x << endl;
-      break;
-    }
-  }
-}
-
-long long length(Point a, Point b) {
-  long long dx = (long long)a.x - b.x;
-  long long dy = (long long)a.y - b.y;
-  long long dz = (long long)a.z - b.z;
-  return dx*dx + dy*dy + dz*dz;
-}
-
-int get_closest_point(vector<Point> points, int index) {
-  long squared_distance = 0;
-  int min_index = -1;
-  for (int i=0; i<points.size(); ++i) {
-    if (i == index) continue;
-    long distance = length(points[index], points[i]);
-    if (min_index < 0) {
-      squared_distance = distance;
-      min_index = i;
-    } else if (distance < squared_distance) {
-      squared_distance = distance;
-      min_index = i;
-    }
-  }
-  return min_index;
-}
-
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
