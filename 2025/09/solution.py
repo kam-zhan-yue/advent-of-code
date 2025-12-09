b5141ec07f65537dc96e4c1a62e6d7296ba4ae92
Alex Kam <kamzhanyue@gmail.com>
2025-12-09 17:51:03 +0900

    finally solve

--- 2025/09/solution.py
+++ 2025/09/solution.py
@@ -15,26 +15,20 @@ class Point:
 class Line:
     p1: Point
     p2: Point
-    horizontal: bool
 
     def __init__(self, p1: Point, p2: Point):
-        self.horizontal = p1.y == p2.y
+        self.p1 = Point(min(p1.x, p2.x), min(p1.y, p2.y))
+        self.p2 = Point(max(p1.x, p2.x), max(p1.y, p2.y))
 
-        # This should be given by the test cases
-        if not self.horizontal:
-            assert(p1.x == p2.x)
-
-        # if horizontal, then put the smaller x to p1
-        if self.horizontal:
-            self.p1 = p1 if p1.x < p2.x else p2
-            self.p2 = p1 if p1.x > p2.x else p2
-        # If vertical, then put the smaller y to p1
-        else:
-            self.p1 = p1 if p1.y < p2.y else p2
-            self.p2 = p1 if p1.y > p2.y else p2
+    def intersects(self, rect: "Rect"):
+        x, x2 = sorted([rect.p1.x, rect.p2.x])
+        y, y2 = sorted([rect.p1.y, rect.p2.y])
+        intersects_x = self.p1.x < x2 and self.p2.x > x
+        intersects_y = self.p1.y < y2 and self.p2.y > y
+        return intersects_x and intersects_y
 
     def __str__(self):
-        return f"{"H" if self.horizontal else "V"} {self.p1} - {self.p2})"
+        return f"({self.p1}, {self.p2})"
 
 class Rect:
     area: int
@@ -61,66 +55,18 @@ def get_points(lines: list[str]) -> list[Point]:
         points.append(Point(int(splits[0]), int(splits[1])))
     return points
 
-def intersects(l1: Line, l2: Line) -> bool:
-    """
-    Checks whether l1 is intersected by l2
-    """
-    # Don't compare parallel lines / tangents
-    if (l1.horizontal == l2.horizontal):
-        print("OH NO!", l1, l2)
-    assert(l1.horizontal != l2.horizontal)
-    # Compare a -- with a |
-    if l1.horizontal and not l2.horizontal:
-        y = l1.p1.y # should be the same as l1.p2.y
-        x = l2.p1.x # should be the same as l2.p2.x
-
-        within_y = l2.p1.y <= y and l2.p2.y >= y
-        within_x = l1.p1.x < x and l1.p2.x > x
-        return within_y and within_x
-    elif not l1.horizontal and l2.horizontal:
-        x = l1.p1.x # should be the same as l1.p2.x
-        y = l2.p1.y # should be the same as l2.p2.y
-
-        within_x = l2.p1.x <= x and l2.p2.x >= x
-        within_y = l1.p1.y < y and l1.p2.y > y
-        return within_y and within_x
-
-    # This should never be reached
-    print("OH NO!")
+def is_valid(rect: Rect, lines: list[Line]):
+    # If a line intersects the rectangle, then it is invalid
+    for line in lines:
+        if line.intersects(rect):
+            return False
     return True
 
-def is_rect_legal(rect: Rect, lines: list[Line]) -> bool:
-    p1, p2 = rect.p1, rect.p2
-    x1, x2 = min(p1.x, p2.x), max(p1.x, p2.x)
-    y1, y2 = min(p1.y, p2.y), max(p1.y, p2.y)
-    left = Line(Point(x1, y1), Point(x1, y2))
-    right = Line(Point(x2, y1), Point(x2, y2))
-    top = Line(Point(x1, y1), Point(x2, y1))
-    bottom = Line(Point(x1, y1), Point(x2, y1))
-
-    # Force set because the points can be the same
-    left.horizontal = False
-    right.horizontal = False
-    top.horizontal = True
-    bottom.horizontal = True
-
-    for i in range(len(lines)):
-        line = lines[i]
-        if line.horizontal:
-            if intersects(left, line) or intersects(right, line):
-                return False
-        else:
-            if intersects(top, line) or intersects(bottom, line):
-                return False
-
-    print(f"{rect} is legal")
-    return True
-    
 def solve(points: list[Point]):
     rectangles: list[Rect] = []
     lines: list[Line] = []
     for i in range(len(points)):
-        next = i + 1 if i < len(points) - 1 else 0
+        next = i + 1 if i + 1 < len(points) else 0
         lines.append(Line(points[i], points[next]))
 
         for j in range(i + 1, len(points)):
@@ -130,7 +76,7 @@ def solve(points: list[Point]):
 
     max_area = 0
     for i in range(len(rectangles)):
-        if is_rect_legal(rectangles[i], lines):
+        if is_valid(rectangles[i], lines):
             max_area = rectangles[i].area
             break
 
@@ -142,5 +88,3 @@ test_points = get_points(test_input)
 main_points = get_points(main_input)
 solve(test_points)
 solve(main_points)
-
-
