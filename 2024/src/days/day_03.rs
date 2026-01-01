90a56a368caabdddef414b2cbe82c594dcd5cf9f
Alex Kam <kamzhanyue@gmail.com>
2026-01-02 00:32:04 +0900

    start on day 3

--- 2024/src/days/day_03.rs
+++ 2024/src/days/day_03.rs
@@ -1,13 +1,48 @@
+use regex::Regex;
+
 pub fn solve(input: &str) {
-    let mut x = 0i32;
+    let mut p1 = 0i32;
+    let mut p2 = 0i32;
     for line in input.lines() {
-        x += part_one(line);
+        p1 += part_one(line);
+        p2 += part_one(line);
     }
-    println!("Part One is {}", x);
+    println!("Part One is {}", p1);
+    println!("Part Two is {}", p2);
 }
 
 fn part_one(line: &str) -> i32 {
-    line.len() as i32
+    let mut sum = 0i32;
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let captures = re.captures_iter(line);
+    for capture in captures {
+        let left = &capture[1];
+        let right = &capture[2];
+        let left_error = format!("{left} is not a number");
+        let right_error = format!("{right} is not a number");
+        let l = left.parse::<i32>().expect(&left_error);
+        let r = right.parse::<i32>().expect(&right_error);
+        sum += l * r;
+    }
+    sum
+}
+
+fn part_two(line: &str) -> i32 {
+    let mut sum = 0i32;
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let enable = Regex::new(r"do\(\)").unwrap();
+    let disable = Regex::new(r"don't\(\)").unwrap();
+    let captures = re.captures_iter(line);
+    for capture in captures {
+        let left = &capture[1];
+        let right = &capture[2];
+        let left_error = format!("{left} is not a number");
+        let right_error = format!("{right} is not a number");
+        let l = left.parse::<i32>().expect(&left_error);
+        let r = right.parse::<i32>().expect(&right_error);
+        sum += l * r;
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -21,6 +56,7 @@ mod tests {
 
     #[test]
     fn test_part_two() {
+        assert_eq!(part_two("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"), 48);
     }
 }
 
